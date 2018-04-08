92d1e928535307313736ad15a63b84fa6b0bb8df
zzZDenisZzz <corey2007@bigmir.net>
2018-04-08 09:59:57 +0300

    Created onConfigurationChanged (SelectedVideo v0.6)

--- app/src/main/java/com/kiev/corey/androidmediaplayer/SelectedVideo.java
+++ app/src/main/java/com/kiev/corey/androidmediaplayer/SelectedVideo.java
@@ -1,12 +1,15 @@
 package com.kiev.corey.androidmediaplayer;
 
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -17,7 +20,7 @@ import static android.os.Environment.DIRECTORY_MOVIES;
 public class SelectedVideo extends AppCompatActivity {
 
     VideoView videoView;
-    Button play;
+    Button back, stop, play, next;
     String itemValue;
     File directory;
     MediaController mediaController;
@@ -29,6 +32,10 @@ public class SelectedVideo extends AppCompatActivity {
 
         videoView = findViewById(R.id.video_View);
         play = findViewById(R.id.btn_play);
+        stop = findViewById(R.id.btn_stop);
+        play = findViewById(R.id.btn_play);
+        play = findViewById(R.id.btn_play);
+
 
         Bundle bundle = getIntent().getExtras();
 
@@ -38,16 +45,63 @@ public class SelectedVideo extends AppCompatActivity {
         directory = Environment.getExternalStoragePublicDirectory(DIRECTORY_MOVIES);
 
         mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                videoView.pause();
+            }
+        });
 
         play.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Uri uri = Uri.parse(directory+"/" + itemValue);
+                Uri uri = Uri.parse(directory + "/" + itemValue);
                 videoView.setVideoURI(uri);
-                videoView.setMediaController(mediaController);
                 videoView.start();
             }
         });
 
 
+//        play.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Uri uri = Uri.parse(directory+"/" + itemValue);
+//                videoView.setVideoURI(uri);
+//                videoView.setMediaController(mediaController);
+//                videoView.start();
+//            }
+//        });
+
+    }
+
+    // Изменение конфигурации для "ORIENTATION LANDSCAPE"
+    LinearLayout.LayoutParams paramsNotFullscreen; // если использую LinearLayout
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) //В полноэкранном режиме
+        {
+            paramsNotFullscreen=(LinearLayout.LayoutParams)videoView.getLayoutParams();
+            LinearLayout.LayoutParams params= null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                params = new LinearLayout.LayoutParams(paramsNotFullscreen);
+            }
+            if (params != null) {
+                params.setMargins(0, 0, 0, 0);
+                params.height=ViewGroup.LayoutParams.MATCH_PARENT;
+                params.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            }
+
+            videoView.setLayoutParams(params);
+
+        }
+        else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            videoView.setLayoutParams(paramsNotFullscreen);
+        }
     }
 }
